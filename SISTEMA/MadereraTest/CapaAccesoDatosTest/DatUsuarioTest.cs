bde8b1e67cba0c4b7e1c650ce213ef0cb78f17ae
mike <mike.ur.addict@gmail.com>
2023-05-25 00:11:13 -0500

    agegrar test unitario de crear cliente

---
+++ SISTEMA/MadereraTest/CapaAccesoDatosTest/DatUsuarioTest.cs
@@ -0,0 +1,62 @@
+using CapaAccesoDatos;
+using CapaAccesoDatos.Interfaces;
+using CapaEntidad;
+using CapaLogica;
+using Moq;
+using System.Runtime.CompilerServices;
+
+namespace MadereraTest.CapaAccesoDatosTest
+
+{
+    public class Tests
+    {
+
+        [Test]
+        public void CrearUsuarioCorrecto()
+        {
+            var mock = new Mock<IDatUsuario>();
+            var newusuario = new EntUsuario
+            {
+                RazonSocial = "carlos",
+                UserName = "carlos#1",
+                Pass = "123",
+                Roll = new entRoll
+                {
+                    IdRoll = 3
+                },
+                Ubigeo = new EntUbigeo
+                {
+                    IdUbigeo = "010109"
+                }
+            };
+            mock.Setup(o => o.CrearCliente(newusuario)).Returns(true);
+            var logusurio = new LogUsuario(mock.Object);
+            var creado = logusurio.CrearClientes(newusuario);
+            Assert.That(creado, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void CrearUsuarioIncorrecto()
+        {
+            var mock = new Mock<IDatUsuario>();
+            var newusuario = new EntUsuario
+            {
+                RazonSocial = "carlos",
+                UserName = "carlos#1",
+                Pass = "123",
+                Roll = new entRoll
+                {
+                    IdRoll = 3
+                },
+                Ubigeo = new EntUbigeo
+                {
+                    IdUbigeo = "010109457755"
+                }
+            };
+            mock.Setup(o => o.CrearCliente(newusuario)).Returns(false);
+            var logusurio = new LogUsuario(mock.Object);
+            var creado = logusurio.CrearClientes(newusuario);
+            Assert.That(creado, Is.EqualTo(false));
+        }
+    }
+}
