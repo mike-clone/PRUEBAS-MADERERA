51ce4b8e17fff4d762066ee74fa96e4ae3b882b2
Nicole Salazar <nist.jhs@gmail.com>
2023-05-29 23:56:01 -0500

    Agregada pruebas DatVenta

--- SISTEMA/MadereraTest/CapaAccesoDatosTest/DatVentaTest.cs
+++ SISTEMA/MadereraTest/CapaAccesoDatosTest/DatVentaTest.cs
@@ -2,11 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CapaAccesoDatos.Interfaces;
+using CapaEntidad;
+using CapaLogica;
+using Moq;
 using System.Threading.Tasks;
 
 namespace MadereraTest.CapaAccesoDatosTest
 {
-    internal class DatVentaTest
+    public class DatVentaTest
     {
+        [Test]
+        public void MostrarVentaCaso01()
+        {
+            var mock = new Mock<IDatVenta>();
+            var venta = new List<EntVenta>
+            {
+                new EntVenta 
+                { 
+                    Cliente = new EntUsuario
+                    {
+                        IdUsuario = 4
+                    },
+                    Fecha = new DateTime(2023,5,29),
+                    Total = 45.5
+                },
+            };
+
+            mock.Setup(o => o.ListarVenta(1)).Returns(venta);
+            var logVenta = new LogVenta(mock.Object);
+            var mostrado = logVenta.ListarVenta(1);
+            Assert.IsNotNull(mostrado);
+        }
+
+        [Test]
+        public void MostrarVentaCaso02()
+        {
+            var mock = new Mock<IDatVenta>();
+            var venta = new List<EntVenta>
+            {
+                new EntVenta
+                {
+                    Cliente = new EntUsuario
+                    {
+                        IdUsuario = 6
+                    },
+                    Fecha = new DateTime(2023,5,29),
+                    Total = 15
+                },
+            };
+
+            mock.Setup(o => o.ListarVenta(3)).Returns(venta);
+            var logVenta = new LogVenta(mock.Object);
+            var mostrado = logVenta.ListarVenta(3);
+            Assert.IsNotNull(mostrado);
+        }
+
+        [Test]
+        public void MostrarVentaCaso03()
+        {
+            var mock = new Mock<IDatVenta>();
+            var venta = new List<EntVenta>();
+
+            mock.Setup(o => o.ListarVenta(5)).Returns(venta);
+            var logVenta = new LogVenta(mock.Object);
+            var mostrado = logVenta.ListarVenta(5);
+            Assert.IsEmpty(mostrado);
+        }
     }
 }
